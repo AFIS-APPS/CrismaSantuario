c80b2796ad320c41631e44b85129b718a11fa5e3
afis <afis.apps@gmail.com>
2018-07-18 21:56:32 -0500

    Commit inicial

---
+++ app/src/main/java/com/apps/afis/crismasantuario/Cadastro/CadastroCatequistaActivity.java
@@ -0,0 +1,15 @@
+package com.apps.afis.crismasantuario.Cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.afis.crismasantuario.R;
+
+public class CadastroCatequistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_catequista);
+    }
+}
--- app/src/main/java/com/apps/afis/crismasantuario/Main/SplashScreen.java
+++ app/src/main/java/com/apps/afis/crismasantuario/Main/SplashScreen.java
@@ -1,13 +1,15 @@
-package com.apps.afis.crismasantuario;
+package com.apps.afis.crismasantuario.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.apps.afis.crismasantuario.R;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
